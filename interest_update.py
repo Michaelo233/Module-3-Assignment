99d4a41ac9c4803a7a7deab46b59599e3e500f38
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-02-16 03:41:35 -0600

    Interest Calculation: Account balance yearly interest calculation.

--- interest_update.py
+++ interest_update.py
@@ -12,3 +12,34 @@ with open("account_balances.txt", "r") as account_balance_file:
       key, value = line.strip().split('|')
       users_account_balances[key] = float(value)
 pprint(users_account_balances)
+print("\n")
+
+for key, value in users_account_balances.items():
+   if value >= 1000:
+      #print(value)
+      balance = (value * 2.5) / (12 * 100)
+      value += balance
+      users_account_balances[key] = value
+      
+   elif value < 1000:
+      balance = (value * 1) / (12 * 100)
+      value += balance
+      users_account_balances[key] = value
+
+   elif value >= 5000:
+      balance = (value * 5) / (12 * 100)
+      value += balance
+      users_account_balances[key] = value
+
+   elif value == 0:
+      value = value
+      users_account_balances[key] = value
+
+   else:
+      balance = (value * 10) / (12 * 100)
+      value += balance
+      users_account_balances[key] = value
+pprint(users_account_balances)
+
+account_balance_file.close()
+
